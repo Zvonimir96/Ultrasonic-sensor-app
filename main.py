a12983b1f4d341a3fc37b88e4275210a9e675f3c
Zvonimir <zlavric@tvz.hr>
2023-07-03 10:39:47 +0200

    Documentation

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import serial.tools.list_ports
 
 v_z = 340.3
 broj_decimala = 4
+h = 0
 
 event = threading.Event()
 event.set()
-h = 0
 
 
 def calculate_distance(vrijeme, v_z):
@@ -19,8 +19,8 @@ def calculate_distance(vrijeme, v_z):
     return s
 
 
-def temperature_correction(temperatura, vrijeme):
-    v_z_kor = 331.3+0.606*temperatura
+def temperature_correction(temperature, vrijeme):
+    v_z_kor = 331.3+0.606*temperature
     s = calculate_distance(vrijeme, v_z_kor)
     return s
 
@@ -31,8 +31,8 @@ def geometry_correction(h, v_z, vrijeme):
     return d
 
 
-def geo_and_temp_correction(temperatura, vrijeme, h):
-     s = temperature_correction(temperatura, vrijeme)
+def geo_and_temp_correction(temperature, vrijeme, h):
+     s = temperature_correction(temperature, vrijeme)
      d = math.sqrt(s ** 2 - h ** 2)
      return d
 
@@ -42,13 +42,14 @@ class Worker(QObject):
     set_data = pyqtSignal(float, int)
 
     def do_work(self):
-        while(1):
+        while(True):
             running = True
             error = False
 
             while (running):
                 ports = serial.tools.list_ports.comports()
 
+                # Find STM32 serial port by its name
                 STM_port = ''
                 for port in ports:
                     try:
@@ -59,6 +60,7 @@ class Worker(QObject):
                         STM_port = ''
 
                 try:
+                    # Set serial communication parameters
                     serialInst = serial.Serial()
 
                     serialInst.baudrate = 9600
@@ -68,24 +70,27 @@ class Worker(QObject):
                     error = False
                 except Exception:
                     if not error:
+                        # Display error message
                         self.connection_error.emit()
 
                     error = True
 
                 else:
+                    # Serial communication is set up correctly
                     while (running):
                         try:
                             if serialInst.in_waiting:
                                 packet = serialInst.readline()
 
+                                # Read and decode data
                                 message = packet.decode('utf').rstrip('\n')
                                 data = message.split("; ")
 
+                                # Display data
                                 self.set_data.emit(float(data[1]), int(data[0]))
 
-
-
                         except Exception:
+                            # Display error message
                             self.connection_error.emit()
                             error = True
 
@@ -94,109 +99,109 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setFixedSize(720, 170)
 
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
+        self.central_widget = QtWidgets.QWidget(MainWindow)
+        self.central_widget.setObjectName("centralwidget")
 
-        # Definiranje fonta
+        # Font
         group_font = QtGui.QFont()
         group_font.setPointSize(14)
 
         label_font = QtGui.QFont()
         label_font.setPointSize(12)
 
-        # Group udaljenost
-        self.udaljenost = QtWidgets.QGroupBox(self.centralwidget)
-        self.udaljenost.setGeometry(QtCore.QRect(10, 0, 270, 150))
-        self.udaljenost.setFont(group_font)
+        # Group distance
+        self.distance = QtWidgets.QGroupBox(self.central_widget)
+        self.distance.setGeometry(QtCore.QRect(10, 0, 270, 150))
+        self.distance.setFont(group_font)
 
-        # Udaljenost label
-        self.label_temperaturna = QtWidgets.QLabel(self.udaljenost)
-        self.label_temperaturna.setGeometry(QtCore.QRect(10, 60, 131, 21))
-        self.label_temperaturna.setFont(label_font)
-        self.label_temperaturna.setObjectName("label_temperaturna")
+        # Distance label
+        self.label_temperature = QtWidgets.QLabel(self.distance)
+        self.label_temperature.setGeometry(QtCore.QRect(10, 60, 131, 21))
+        self.label_temperature.setFont(label_font)
+        self.label_temperature.setObjectName("label_temperaturna")
 
-        self.label_obje_korekcije = QtWidgets.QLabel(self.udaljenost)
-        self.label_obje_korekcije.setGeometry(QtCore.QRect(16, 120, 120, 20))
-        self.label_obje_korekcije.setFont(label_font)
-        self.label_obje_korekcije.setObjectName("label_obje_korekcije")
+        self.label_both_corrections = QtWidgets.QLabel(self.distance)
+        self.label_both_corrections.setGeometry(QtCore.QRect(16, 120, 120, 20))
+        self.label_both_corrections.setFont(label_font)
+        self.label_both_corrections.setObjectName("label_obje_korekcije")
 
-        self.label_geometrijska = QtWidgets.QLabel(self.udaljenost)
-        self.label_geometrijska.setGeometry(QtCore.QRect(10, 90, 131, 21))
-        self.label_geometrijska.setFont(label_font)
-        self.label_geometrijska.setObjectName("label_geometrijska")
+        self.label_geometry_correction = QtWidgets.QLabel(self.distance)
+        self.label_geometry_correction.setGeometry(QtCore.QRect(10, 90, 131, 21))
+        self.label_geometry_correction.setFont(label_font)
+        self.label_geometry_correction.setObjectName("label_geometrijska")
 
-        self.label_bez_korekcije = QtWidgets.QLabel(self.udaljenost)
-        self.label_bez_korekcije.setGeometry(QtCore.QRect(30, 30, 101, 21))
-        self.label_bez_korekcije.setFont(label_font)
-        self.label_bez_korekcije.setObjectName("label_bez_korekcije")
+        self.label_without_corrcetion = QtWidgets.QLabel(self.distance)
+        self.label_without_corrcetion.setGeometry(QtCore.QRect(30, 30, 101, 21))
+        self.label_without_corrcetion.setFont(label_font)
+        self.label_without_corrcetion.setObjectName("label_bez_korekcije")
 
-        # Udaljenost text
-        self.text_obje_korekcije = QtWidgets.QLineEdit(self.udaljenost)
-        self.text_obje_korekcije.setGeometry(QtCore.QRect(150, 120, 113, 22))
-        self.text_obje_korekcije.setEnabled(False)
+        # Distance text
+        self.text_both_corrections = QtWidgets.QLineEdit(self.distance)
+        self.text_both_corrections.setGeometry(QtCore.QRect(150, 120, 113, 22))
+        self.text_both_corrections.setEnabled(False)
 
-        self.text_temperaturna = QtWidgets.QLineEdit(self.udaljenost)
-        self.text_temperaturna.setGeometry(QtCore.QRect(150, 60, 113, 22))
-        self.text_temperaturna.setEnabled(False)
+        self.text_temperature = QtWidgets.QLineEdit(self.distance)
+        self.text_temperature.setGeometry(QtCore.QRect(150, 60, 113, 22))
+        self.text_temperature.setEnabled(False)
 
-        self.text_geometrijska = QtWidgets.QLineEdit(self.udaljenost)
-        self.text_geometrijska.setGeometry(QtCore.QRect(150, 90, 113, 22))
-        self.text_geometrijska.setEnabled(False)
+        self.text_geometry_correction = QtWidgets.QLineEdit(self.distance)
+        self.text_geometry_correction.setGeometry(QtCore.QRect(150, 90, 113, 22))
+        self.text_geometry_correction.setEnabled(False)
 
-        self.text_bez_korekcije = QtWidgets.QLineEdit(self.udaljenost)
-        self.text_bez_korekcije.setGeometry(QtCore.QRect(150, 30, 113, 22))
-        self.text_bez_korekcije.setEnabled(False)
+        self.text_without_corrcetion = QtWidgets.QLineEdit(self.distance)
+        self.text_without_corrcetion.setGeometry(QtCore.QRect(150, 30, 113, 22))
+        self.text_without_corrcetion.setEnabled(False)
 
-        # Grupa brzina zvuka
-        self.brzina_zvuka = QtWidgets.QGroupBox(self.centralwidget)
-        self.brzina_zvuka.setGeometry(QtCore.QRect(290, 0, 280, 90))
-        self.brzina_zvuka.setFont(group_font)
+        # Group sound speed
+        self.sound_speed = QtWidgets.QGroupBox(self.central_widget)
+        self.sound_speed.setGeometry(QtCore.QRect(290, 0, 280, 90))
+        self.sound_speed.setFont(group_font)
 
-        # Brzina zvuka label
-        self.label_vz_temp = QtWidgets.QLabel(self.brzina_zvuka)
+        # Sound speed label
+        self.label_vz_temp = QtWidgets.QLabel(self.sound_speed)
         self.label_vz_temp.setGeometry(QtCore.QRect(10, 30, 91, 21))
         self.label_vz_temp.setFont(label_font)
 
-        self.label_vz_trenutna = QtWidgets.QLabel(self.brzina_zvuka)
-        self.label_vz_trenutna.setGeometry(QtCore.QRect(10, 60, 141, 21))
-        self.label_vz_trenutna.setFont(label_font)
+        self.label_vz_current = QtWidgets.QLabel(self.sound_speed)
+        self.label_vz_current.setGeometry(QtCore.QRect(10, 60, 141, 21))
+        self.label_vz_current.setFont(label_font)
 
-        # Brzina zvuka text
-        self.text_trenutna = QtWidgets.QLineEdit(self.brzina_zvuka)
-        self.text_trenutna.setGeometry(QtCore.QRect(150, 60, 113, 22))
-        self.text_trenutna.setEnabled(False)
+        # Sound speed text
+        self.text_vz_current = QtWidgets.QLineEdit(self.sound_speed)
+        self.text_vz_current.setGeometry(QtCore.QRect(150, 60, 113, 22))
+        self.text_vz_current.setEnabled(False)
 
-        self.text_temp = QtWidgets.QLineEdit(self.brzina_zvuka)
+        self.text_temp = QtWidgets.QLineEdit(self.sound_speed)
         self.text_temp.setGeometry(QtCore.QRect(150, 30, 113, 22))
         self.text_temp.setEnabled(False)
 
-        # Grupa T i R razmak
-        self.T_R_razmak = QtWidgets.QGroupBox(self.centralwidget)
-        self.T_R_razmak.setGeometry(QtCore.QRect(290, 90, 281, 61))
-        self.T_R_razmak.setFont(group_font)
+        # Group T i R space
+        self.T_R_space = QtWidgets.QGroupBox(self.central_widget)
+        self.T_R_space.setGeometry(QtCore.QRect(290, 90, 281, 61))
+        self.T_R_space.setFont(group_font)
 
-        self.text_udaljenost = QtWidgets.QLineEdit(self.T_R_razmak)
-        self.text_udaljenost.setGeometry(QtCore.QRect(199, 25, 71, 25))
-        self.text_udaljenost.setEnabled(False)
-        self.text_udaljenost.setText("0 mm")
+        self.text_distance = QtWidgets.QLineEdit(self.T_R_space)
+        self.text_distance.setGeometry(QtCore.QRect(199, 25, 71, 25))
+        self.text_distance.setEnabled(False)
+        self.text_distance.setText("0 mm")
 
-        self.slider_udaljenost = QtWidgets.QSlider(self.T_R_razmak)
-        self.slider_udaljenost.setGeometry(QtCore.QRect(10, 30, 181, 20))
-        self.slider_udaljenost.setMaximum(70)
-        self.slider_udaljenost.setOrientation(QtCore.Qt.Horizontal)
-        self.slider_udaljenost.valueChanged.connect(self.sliderChanged)
-        self.slider_udaljenost.sliderReleased.connect(self.sliderRelese)
+        self.slider_distance = QtWidgets.QSlider(self.T_R_space)
+        self.slider_distance.setGeometry(QtCore.QRect(10, 30, 181, 20))
+        self.slider_distance.setMaximum(70)
+        self.slider_distance.setOrientation(QtCore.Qt.Horizontal)
+        self.slider_distance.valueChanged.connect(self.slider_changed)
+        self.slider_distance.sliderReleased.connect(self.slider_release)
 
-        # Grupa temperatura
-        self.temperatura = QtWidgets.QGroupBox(self.centralwidget)
-        self.temperatura.setGeometry(QtCore.QRect(580, 0, 131, 151))
-        self.temperatura.setFont(group_font)
+        # Group temperature
+        self.temperature = QtWidgets.QGroupBox(self.central_widget)
+        self.temperature.setGeometry(QtCore.QRect(580, 0, 131, 151))
+        self.temperature.setFont(group_font)
 
-        self.text_bmp = QtWidgets.QLineEdit(self.temperatura)
+        self.text_bmp = QtWidgets.QLineEdit(self.temperature)
         self.text_bmp.setGeometry(QtCore.QRect(30, 120, 80, 25))
         self.text_bmp.setEnabled(False)
 
-        self.porgress_temp = QtWidgets.QProgressBar(self.temperatura)
+        self.porgress_temp = QtWidgets.QProgressBar(self.temperature)
         self.porgress_temp.setGeometry(QtCore.QRect(60, 30, 21, 81))
         self.porgress_temp.setMinimum(10)
         self.porgress_temp.setMaximum(40)
@@ -204,7 +209,7 @@ class Ui_MainWindow(object):
         self.porgress_temp.setOrientation(QtCore.Qt.Vertical)
 
         # Menu bar
-        MainWindow.setCentralWidget(self.centralwidget)
+        MainWindow.setCentralWidget(self.central_widget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 714, 21))
         MainWindow.setMenuBar(self.menubar)
@@ -225,66 +230,65 @@ class Ui_MainWindow(object):
 
         self.worker_thread.start()
 
-
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Senzori - mjerenje udaljenosti ultrazvučnog senzora"))
-        self.udaljenost.setTitle(_translate("MainWindow", "Udaljenost"))
-        self.label_temperaturna.setText(_translate("MainWindow", "S temperaturnom"))
-        self.label_obje_korekcije.setText(_translate("MainWindow", "S obje korekcije"))
-        self.label_geometrijska.setText(_translate("MainWindow", "S geometrijskom"))
-        self.label_bez_korekcije.setText(_translate("MainWindow", "Bez korekcije"))
-        self.brzina_zvuka.setTitle(_translate("MainWindow", "Brzina zvuka"))
+        self.distance.setTitle(_translate("MainWindow", "Udaljenost"))
+        self.label_temperature.setText(_translate("MainWindow", "S temperaturnom"))
+        self.label_both_corrections.setText(_translate("MainWindow", "S obje korekcije"))
+        self.label_geometry_correction.setText(_translate("MainWindow", "S geometrijskom"))
+        self.label_without_corrcetion.setText(_translate("MainWindow", "Bez korekcije"))
+        self.sound_speed.setTitle(_translate("MainWindow", "Brzina zvuka"))
         self.label_vz_temp.setText(_translate("MainWindow", "Vz - 15.5°C"))
-        self.label_vz_trenutna.setText(_translate("MainWindow", "Vz - trenutna temp"))
-        self.T_R_razmak.setTitle(_translate("MainWindow", "T i R razmak"))
-        self.temperatura.setTitle(_translate("MainWindow", "Temperatura"))
+        self.label_vz_current.setText(_translate("MainWindow", "Vz - trenutna temp"))
+        self.T_R_space.setTitle(_translate("MainWindow", "T i R razmak"))
+        self.temperature.setTitle(_translate("MainWindow", "temperature"))
 
-    def sliderChanged(self):
-        self.text_udaljenost.setText(f"{self.slider_udaljenost.value()} mm")
+    def slider_changed(self):
+        self.text_distance.setText(f"{self.slider_distance.value()} mm")
 
-    def sliderRelese(self):
+    def slider_release(self):
         if event.is_set():
             event.clear()
 
             global h
-            h = int(self.slider_udaljenost.value())/10
+            h = int(self.slider_distance.value())/10
 
             event.set()
 
     def no_connection(self):
-        self.text_obje_korekcije.setText("None")
-        self.text_temperaturna.setText("None")
-        self.text_geometrijska.setText("None")
-        self.text_bez_korekcije.setText("None")
-        self.text_trenutna.setText("None")
+        self.text_both_corrections.setText("None")
+        self.text_temperature.setText("None")
+        self.text_geometry_correction.setText("None")
+        self.text_without_corrcetion.setText("None")
+        self.text_vz_current.setText("None")
         self.text_temp.setText("None")
         self.text_bmp.setText("None")
         self.porgress_temp.setProperty("value", 0)
 
-    def set_data(self, temperatura, udaljenost):
+    def set_data(self, temperature, udaljenost):
         if event.is_set():
             event.clear()
 
-            # Calculate udaljenost
-            udaljenost_bez_korekcije = calculate_distance(udaljenost, v_z)
-            udaljenost_s_temp_korekcijom = temperature_correction(temperatura, udaljenost)
-            udaljenost_s_geo_korekcijom = geometry_correction(h, v_z, udaljenost)
-            udaljenost_s_obje_korekcije = geo_and_temp_correction(temperatura, udaljenost, h)
+            # Calculate distance
+            distance_without_correction = calculate_distance(udaljenost, v_z)
+            distance_with_temperature_correction = temperature_correction(temperature, udaljenost)
+            distance_with_geometry_correction = geometry_correction(h, v_z, udaljenost)
+            distance_with_corrections = geo_and_temp_correction(temperature, udaljenost, h)
 
-            # Set temperatura
-            self.text_bmp.setText("{:.2f}".format(temperatura) + ' °C')
-            self.porgress_temp.setProperty("value", temperatura)
+            # Set temperature
+            self.text_bmp.setText("{:.2f}".format(temperature) + ' °C')
+            self.porgress_temp.setProperty("value", temperature)
 
-            # Set udaljenosti
-            self.text_bez_korekcije.setText("{:.2f}".format(udaljenost_bez_korekcije) + ' cm')
-            self.text_temperaturna.setText("{:.2f}".format(udaljenost_s_temp_korekcijom) + ' cm')
-            self.text_geometrijska.setText("{:.2f}".format(udaljenost_s_geo_korekcijom) + ' cm')
-            self.text_obje_korekcije.setText("{:.2f}".format(udaljenost_s_obje_korekcije) + ' cm')
+            # Set distance
+            self.text_without_corrcetion.setText("{:.2f}".format(distance_without_correction) + ' cm')
+            self.text_temperature.setText("{:.2f}".format(distance_with_temperature_correction) + ' cm')
+            self.text_geometry_correction.setText("{:.2f}".format(distance_with_geometry_correction) + ' cm')
+            self.text_both_corrections.setText("{:.2f}".format(distance_with_corrections) + ' cm')
 
-            # Set brzina zvuka
-            v_z_kor = 331.3 + 0.606 * temperatura
-            self.text_trenutna.setText("{:.2f}".format(v_z_kor) + ' m/s')
+            # Set sound speed
+            v_z_kor = 331.3 + 0.606 * temperature
+            self.text_vz_current.setText("{:.2f}".format(v_z_kor) + ' m/s')
             self.text_temp.setText("{:.2f}".format(v_z) + ' m/s')
 
             event.set()
